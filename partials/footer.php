2a8dd2b68cd39d078d8b2a9060b7357d010f1505
Hector Quintero <hectorq9826@gmail.com>
2019-11-21 18:16:06 -0500

    asd

--- partials/footer.php
+++ partials/footer.php
@@ -6,6 +6,6 @@
                 <img align="float-left" src="../img/logo.png" alt="Logo" class="responsivepie" width="40" height="40">
         </div>
         <div class="col-md-4 mt-3">
-            <span>Universidad Tecnológica de Panamá - Todos los derechos reservados <br>Creado por <i class="fa fa-bug"></i> MARHA <i class="fa fa-bug"></i></span>
+            <span>Universidad Tecnológica de Panamá - Todos los derechos reservados <br>Creado por  MARHA </span>
         </div>
 </footer> 
