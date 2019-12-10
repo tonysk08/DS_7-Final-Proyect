37f46ba9d68dbdfd82cc3dda540e2f0e7fbf25f9
Hector Quintero <hectorq9826@gmail.com>
2019-12-10 00:53:53 -0500

    landing page y css

---
+++ php/landingPage.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="../css/landingPage.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div class="accordion">
+  <ul>
+    <li><a href="../index.php">
+      <div>
+        <h1>Iniciar Sesión</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut consequat.</p>
+      </div>
+      </a> </li>
+    <li><a href="solicitudesPublicas.php">
+      <div>
+        <h1>Solicitudes Públicas</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+      </a> </li>
+    <li><a href="dashboard.php">
+      <div>
+        <h1>Dashboard</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </div>
+      </a> </li>
+  </ul>
+</div>
+</body>
+</html>
